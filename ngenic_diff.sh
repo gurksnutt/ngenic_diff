13e64c10ef7bbcf67b8f6b71431a8e1424bd4314
gurksnutt <stefan.brunosson@rajraj.org>
2022-11-17 12:12:13 +0100

    Update ngenic_diff.sh

--- ngenic_diff.sh
+++ ngenic_diff.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# Written to monitor when Ngenic comes in to stock at Tibber store
-# Script that monitors webpage for change and notifies with pushover for mobile notifications.
-# Initial creation of reference webpage, diff changes against it with push notice if changed.
-# Creates new refence webpage upon change to continue monitoring.
-# All actions are logged to local file, schedule run with crontab at desired interval recommended once per minute.
-# Crontab syntax: */1  * * * * /<path>/ngenic_diff.sh 
 LOG_FILE="/<path>/ngenic_diff.log"
 exec 3>&1 1>>${LOG_FILE} 2>&1
 TIME_STAMP=$(date '+%d/%m/%Y %H:%M:%S')
